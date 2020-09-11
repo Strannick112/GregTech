bce167de0819276d5fa5fa95b19d027084c9a776
Strannick112 <43038491+Strannick112@users.noreply.github.com>
2020-09-11 23:46:52 +0300

    Update assembly_line.lua

--- assembly_line/assembly_line.lua
+++ assembly_line/assembly_line.lua
@@ -21,7 +21,7 @@ function init()
     local tmp = file:read("*a")
     file:close()
     local tmp2 = ser.unserialize(tmp)
-    table.insert(recepie, {[file_name] = tmp2})
+    recepie[#recepie+1] = {[file_name] = tmp2})
     print(#recepie)
     for k, v in pairs(recepie[#recepie]) do
       print(key, value)
