6e7ec99d9acc6da8973de8740f0b93e252510961
Strannick112 <43038491+Strannick112@users.noreply.github.com>
2020-09-13 02:24:27 +0300

    Update create_recepie.lua

--- assembly_line/create_recepie.lua
+++ assembly_line/create_recepie.lua
@@ -9,7 +9,7 @@ local inv = comp.inventory_controller
 local assembly_line = {}
 
 function assembly_line.check(file_name, ...)
-  --local args_i = 1
+  local args_i = 1
   local list = {}
   local file = fs.open("/usr/bin/assembly_line/recepies/" .. file_name, "w")
   print(file)
@@ -17,12 +17,12 @@ function assembly_line.check(file_name, ...)
   for i = 1, inv.getInventorySize(sides.top), 1 do
     if(inv.getStackInSlot(sides.top, i) ~= nil) then
       print(inv.getStackInSlot(sides.top, i).label, inv.getStackInSlot(sides.top, i).size)
-      --if(inv.getStackInSlot(sides.top, i).name == "extracells:certustank") then
-        --list[i] = {string.sub(inv.getStackInSlot(sides.top, i).label, 22, string.len(inv.getStackInSlot(sides.top, i).label)), arg[args_i] or -1, "fluid"}
-        --args_i = args_i + 1
-      --else
+      if(inv.getStackInSlot(sides.top, i).name == "extracells:certustank") then
+        list[i] = {string.sub(inv.getStackInSlot(sides.top, i).label, 22, string.len(inv.getStackInSlot(sides.top, i).label)), arg[args_i] or -1, "fluid"}
+        args_i = args_i + 1
+      else
         list[i] = {inv.getStackInSlot(sides.top, i).label, inv.getStackInSlot(sides.top, i).size, "solid"}
-      --end
+      end
       print(list[i])
     end
   end
