f9f73ec322468bfdff7faf870dc6dc17ae4441a5
Strannick112 <43038491+Strannick112@users.noreply.github.com>
2020-09-13 00:31:03 +0300

    Update output_for_first.lua

--- mekanism/output_for_first.lua
+++ mekanism/output_for_first.lua
@@ -9,17 +9,21 @@ while true do
   repeat
     me = me_controller.getItemsInNetwork()
   until(type(me) == "nil")
-  
-  local tmp
-  repeat
-    tmp = me_controller.store(me[1], db.address, 1, 1)
-  until(tmp)
+  if(#me ~= 0) then
+    local tmp
+    repeat
+      tmp = me_controller.store(me[1], db.address, 1, 1)
+    until(tmp)
 
-  print("--------")
-  print("Сейчас обрабатывается: ", min_label)
-  print("В сети его сейчас: ", min)
-  export_bus.setExportConfiguration(2, 1, db.address, 1)
-  db.clear(1)
-  
+    print("--------")
+    print("Сейчас обрабатывается: ", min_label)
+    print("В сети его сейчас: ", min)
+    export_bus.setExportConfiguration(2, 1, db.address, 1)
+    db.clear(1)
+  else
+    print("--------")
+    print("Сейчас ничего обрабатывается: ", min_label)
+    print("В сети его сейчас: IQ Разработчика", min)
+  end
   os.sleep(1)
 end
