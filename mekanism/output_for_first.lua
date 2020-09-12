6dfab172c16a2e60d08b20fe7549aec98081dc7c
Strannick112 <43038491+Strannick112@users.noreply.github.com>
2020-09-13 00:55:26 +0300

    Update output_for_first.lua

--- mekanism/output_for_first.lua
+++ mekanism/output_for_first.lua
@@ -14,6 +14,7 @@ function init()
   end
 end
 
+init()
 while true do
   local me = nil
   repeat
@@ -28,7 +29,7 @@ while true do
     print("--------")
     print("Сейчас обрабатывается: ", min_label)
     print("В сети его сейчас: ", min)
-    export_bus.setExportConfiguration(2, 1, db.address, 1)
+    export_bus.setExportConfiguration(side, 1, db.address, 1)
     db.clear(1)
   else
     print("--------")
